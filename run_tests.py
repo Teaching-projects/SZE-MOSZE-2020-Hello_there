1ff2c9cd7ccdba2a9c6de83fea73c710db30d14c
Botond <talos.boti@gmail.com>
2020-09-23 18:19:42 +0200

    Add run_tests to workflow

---
+++ run_tests.py
@@ -0,0 +1,17 @@
+import subprocess
+import os
+
+
+unitPairs = ['luke.json palpatine.json',
+             'luke.json vader.json',
+             'palpatine.json luke.json',
+             'palpatine.json vader.json',
+             'vader.json luke.json',
+             'vader.json palpatine.json']
+
+
+with open('output.txt', 'w') as output_f:
+    for pair in unitPairs:
+        #os.system('.\main.exe ' + pair + ' >output.txt')
+        subprocess.Popen('.\main.exe ' + pair,
+                         stdout=output_f, stderr=output_f)
