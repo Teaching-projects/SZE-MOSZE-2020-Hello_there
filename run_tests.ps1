71284c2337dc3ff0e0c0aebca7110240cd460699
Botond <talos.boti@gmail.com>
2020-09-24 15:30:55 +0200

    tidy up and add gitignore

--- run_tests.ps1
+++
@@ -1,8 +0,0 @@
-Clear-Content "output.txt"
-
-.\main.exe luke.json palpatine.json     >>output.txt
-.\main.exe luke.json vader.json         >>output.txt
-.\main.exe palpatine.json luke.json     >>output.txt
-.\main.exe palpatine.json vader.json    >>output.txt
-.\main.exe vader.json luke.json         >>output.txt
-.\main.exe vader.json palpatine.json    >>output.txt
