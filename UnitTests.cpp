0636a807dd88ad9410bdc75b6413e43bc0abdb61
Mate <gfellnermate@gmail.com>
2020-10-11 21:47:33 +0200

    add google test workflow

--- UnitTests.cpp
+++ UnitTests.cpp
@@ -1,4 +1,3 @@
-#include "UnitTests.h"
 #include "gtest/gtest.h"
 #include "JsonParser.h"
 
@@ -7,10 +6,4 @@ TEST(ParserTest, CheckReturnedMapSize)
     EXPECT_EQ(3, JsonParser::Parse("vader.json").size());
     EXPECT_EQ(3, JsonParser::Parse("luke.json").size());
     EXPECT_EQ(3, JsonParser::Parse("palpatine.json").size());
-}
-
-int main(int argc, char **argv)
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
 }
