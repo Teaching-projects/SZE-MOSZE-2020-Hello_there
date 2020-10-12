f7fe2cf3b52dcdcca31315f30077e5d48a9ff202
Mate <gfellnermate@gmail.com>
2020-10-12 19:18:48 +0200

    add try catch

--- main.cpp
+++ main.cpp
@@ -6,21 +6,34 @@ int main(int argc, char *argv[])
 {
     std::string fName1 = argv[1];
     std::string fName2 = argv[2];
-    Unit u1 = Unit::ParseUnit(fName1);
-    Unit u2 = Unit::ParseUnit(fName2);
 
-    for (int turn = 0; !u1.IsDead() && !u2.IsDead(); turn++)
+    Unit *u1;
+    Unit *u2;
+
+    try
     {
-        if (turn % 2 == 0)
-            u1.Defend(u2);
-        else
-            u2.Defend(u1);
+        u1 = new Unit(Unit::ParseUnit(fName1));
+        u2 = new Unit(Unit::ParseUnit(fName2));
+    }
+    catch (const std::string s)
+    {
+        std::cout << "File \"" << s << "\" not found!"
+                  << std::endl;
+        return 404;
     }
 
-    if (u1.IsDead())
-        std::cout << u2.GetName() << " wins. Remaining HP: " << u2.GetHp() << std::endl;
+    for (int turn = 0; !u1->IsDead() && !u2->IsDead(); turn++)
+    {
+        if (turn % 2 == 0)
+            u1->Defend(*u2);
+        else
+            u2->Defend(*u1);
+    }
+
+    if (u1->IsDead())
+        std::cout << u2->GetName() << " wins. Remaining HP: " << u2->GetHp() << std::endl;
     else
-        std::cout << u1.GetName() << " wins. Remaining HP: " << u1.GetHp() << std::endl;
+        std::cout << u1->GetName() << " wins. Remaining HP: " << u1->GetHp() << std::endl;
 
     return 0;
 }
