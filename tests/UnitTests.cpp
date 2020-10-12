f4f5c4e566137095ccab90be9858b3987b10fd25
Mate <gfellnermate@gmail.com>
2020-10-12 11:42:08 +0200

    add CMakeLists.txt

--- tests/UnitTests.cpp
+++ tests/UnitTests.cpp
@@ -1,9 +1,15 @@
 #include "gtest/gtest.h"
-#include "JsonParser.h"
+#include "../JsonParser.h"
 
 TEST(ParserTest, CheckReturnedMapSize)
 {
     EXPECT_EQ(3, JsonParser::Parse("vader.json").size());
     EXPECT_EQ(3, JsonParser::Parse("luke.json").size());
     EXPECT_EQ(3, JsonParser::Parse("palpatine.json").size());
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
