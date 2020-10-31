f1ee308f62b5ddb713ca7298ca374c199ad4688b
Holdmedve <40375861+Holdmedve@users.noreply.github.com>
2020-10-31 16:50:16 +0000

    Update gh-pages to output generated at 2db750b

--- search/functions_1.js
+++ search/functions_1.js
@@ -3,7 +3,5 @@ var searchData=
   ['getatkcooldown',['GetAtkCoolDown',['../class_unit.html#a4e73f1b75e707472179a222754af016a',1,'Unit']]],
   ['getdmg',['GetDmg',['../class_unit.html#a17ebec030fc63b246c6e3f96326eb49f',1,'Unit']]],
   ['gethp',['GetHp',['../class_unit.html#a33da6752c748ad80309f17ac07287a61',1,'Unit']]],
-  ['getlvl',['getLvl',['../class_player.html#a7850b4992760d7a2cdc82fb464745c76',1,'Player']]],
-  ['getname',['GetName',['../class_unit.html#a9ddebd39e011bae985c78ba777374fb2',1,'Unit']]],
-  ['getxp',['getXp',['../class_player.html#af02d884e296840e3ba36e8844ae4c20c',1,'Player']]]
+  ['getname',['GetName',['../class_unit.html#a9ddebd39e011bae985c78ba777374fb2',1,'Unit']]]
 ];
