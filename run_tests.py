280674777aa924b59f06cb64c547abfd12879e07
Holdmedve <talos.boti@gmail.com>
2020-11-03 11:38:40 +0100

    bugfix refactor#6

--- run_tests.py
+++ run_tests.py
@@ -5,11 +5,16 @@ import sys
 exe = './' + sys.argv[1]
 output = sys.argv[2]
 
-Scenarios = ['scenario1.json', 'scenario2.json']
+scenarios = ['scenario1.json', 'scenario2.json']
 
-
+'''
 with open(output, 'w') as output_f:
-    for scenario in Scenarios:
+    for scenario in scenarios:
         args = [exe, scenario]
         subprocess.call(args,
                         stdout=output_f, stderr=output_f)
+'''
+
+with open(output, 'w') as output_f:
+    subprocess.call('./main scenario1.json',
+                    stdout=output_f, stderr=output_f)
