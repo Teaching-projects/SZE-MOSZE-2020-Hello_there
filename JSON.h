e9828bd05b90856a111499c6d5a1fde892deec58
BakiHUN <bakoss998@gmail.com>
2020-11-06 19:53:51 +0100

    Fix merging conflicts #2/2

--- JSON.h
+++ JSON.h
@@ -12,7 +12,7 @@ public:
 	JSON(const std::map<std::string, std::any> &);
 	int count(std::string);
 	template <typename T>
-	T get(const std::string &key)
+	T get(const std::string& key)
 	{
 		std::string value = std::any_cast<std::string>(data[key]);
 		std::any converted;
@@ -31,18 +31,9 @@ public:
 		}
 
 		return std::any_cast<T>(converted);
-	}
-	bool operator==(const JSON& lhs, const JSON& rhs) const
-	{
-		if (lhs.data["name"] == rhs.get<std::string>("name") &&
-			lhs.get<int>("hp") == rhs.get<int>("hp") &&
-			lhs.get<int>("dmg") == rhs.get<int>("dmg") &&
-			lhs.get<float>("attackcooldown") == rhs.get<float>("attackcooldown"))
-		{
-			return true;
-		}
-		return false;
-	}
+	};
+	static bool compareJSON(JSON& j1, JSON& j2);
+
 	static JSON parseFromFile(const char *fileName);
 	static JSON parseFromStream(std::ifstream &fileStream);
 	static JSON parseFromString(const std::string &fileContent);
