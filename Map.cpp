e6d846446effdcd67196ebf05089c8cd91d974c7
gfellnerm <gfellnermate@gmail.com>
2020-11-23 22:36:53 +0100

    add get method to map class

--- Map.cpp
+++ Map.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <fstream>
 
-Map::Map(std::string filename)
+Map::Map(const std::string &filename)
 {
     std::ifstream mapstream(filename);
     std::string line;
@@ -10,4 +10,19 @@ Map::Map(std::string filename)
     {
         map.push_back(line);
     }
+}
+
+Map::type Map::get(int x, int y) const
+{
+    if (x >= map.size() || x < 0)
+    {
+        throw WrongIndexException();
+    }
+    else if (y >= map[x].length() || y < 0)
+    {
+        throw WrongIndexException();
+    }
+
+    std::string types = " #";
+    return type(types.find(map[x][y]));
 }
--- Map.h
+++ Map.h
@@ -15,8 +15,39 @@
 class Map
 {
 private:
+    /**
+    * @brief Contains the map, each line is a string.
+    */
     std::vector<std::string> map;
 
 public:
-    Map(std::string filename);
+    /**
+    * @brief The types that a given point of the map can be. 
+    */
+    enum type
+    {
+        Free,
+        Wall
+    };
+
+    /**
+    * @brief Given a txt it returns the generated map.
+    * @param filename Name of the txt file
+    * @return Map
+    */
+    Map(const std::string &filename);
+
+    Map::type get(const int x, const int y) const;
+
+    /**
+    * @brief Custom exception class.
+    */
+    class WrongIndexException : std::exception
+    {
+    public:
+        /**
+        * @brief Constructor for the exception.
+        */
+        WrongIndexException() {}
+    };
 };
