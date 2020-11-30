bedebdc9698db81a573c639226e66b2a26972872
BakiHUN <bakoss998@gmail.com>
2020-11-30 12:32:50 +0100

    Fixing merging errors

--- main.cpp
+++ main.cpp
@@ -15,12 +15,12 @@
 int main(int argc, char **argv)
 {
     Game g("map_1.txt");
-    Hero *h = new Hero("Joe", 500, 500, 0.5, 5, 5, 5, 5.0);
+	Hero* h = new Hero("Joe", 500, 10, 12.0, 2, 30, 10, 2, 1.2, 1);
     g.PutHero(h, 1, 1);
 
-    Monster *m1 = new Monster("Cookie Monster", 10, 10, 10.0f);
-    Monster *m2 = new Monster("Octopus Poppus", 10, 10, 10.0f);
-    Monster *m3 = new Monster("Lucky Bucky", 10, 10, 10.0f);
+    Monster *m1 = new Monster("Cookie Monster", 10, 10, 10.0,5);
+    Monster *m2 = new Monster("Octopus Poppus", 10, 10, 10.0,6);
+    Monster *m3 = new Monster("Lucky Bucky", 10, 10, 10.0,7);
     g.PutMonster(m1, 1, 2);
     g.PutMonster(m2, 1, 2);
     g.PutMonster(m3, 1, 5);
