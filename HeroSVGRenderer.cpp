c2c2c54a5cf9fb9bd0c17204dd08b631f8c1a3e9
Holdmedve <talos.boti@gmail.com>
2020-12-07 16:48:30 +0100

    bugfix in herosvgrenderer

--- HeroSVGRenderer.cpp
+++ HeroSVGRenderer.cpp
@@ -1,13 +1,13 @@
 #include "HeroSVGRenderer.h"
 
-HeroSVGRenderer::HeroSVGRenderer(std::string s) : SVGRenderer(s) {
-
+HeroSVGRenderer::HeroSVGRenderer(std::string s) : SVGRenderer(s)
+{
 }
 
-void HeroSVGRenderer::render(const Game& g) const
+void HeroSVGRenderer::render(const Game &g) const
 {
-	Map* m = g.getMap();
-	Hero* h = g.getHero();
+	Map *m = g.getMap();
+	Hero *h = g.getHero();
 	int rowCount = m->GetRowCount();
 	int colCount = m->GetColCount(0);
 	std::vector<std::pair<int, int>> monsterCoordinates = g.GetMonsterCoordinates();
@@ -15,22 +15,25 @@ void HeroSVGRenderer::render(const Game& g) const
 	int x = h->GetXCoo();
 	int y = h->GetYCoo();
 	int maxSteps = h->getLightRadius() * 2 + 1;
-	if (maxSteps >= colCount) maxSteps = colCount;
+	if (maxSteps >= colCount)
+		maxSteps = colCount;
 	int startRow = x;
 	int startCol = y;
-	for (int i = 0; i < h->getLightRadius() && startRow > 0 && startRow < rowCount; i++) {
+	for (int i = 0; i < h->getLightRadius() && startRow > 0 && startRow < rowCount; i++)
+	{
 		startRow--;
 	}
-	for (int i = 0; i < h->getLightRadius() && startCol > 0 && startCol < colCount; i++) {
+	for (int i = 0; i < h->getLightRadius() && startCol > 0 && startCol < colCount; i++)
+	{
 		startCol--;
 	}
-	int width = (colCount - startCol)*10;
+	int width = (colCount - startCol) * 10;
 	int height = (rowCount - startRow) * 10;
 	std::ofstream stream(outputStreamName);
 	stream << "<svg viewBox=\"0 0 " << width << ' ' << height << "\" xmlns=\"http://www.w3.org/2000/svg\" width=\"" << width << "\" height=\"" << height << "\">\n";
-	for (int x = 0, row = startRow; x < rowCount && row < rowCount; x++)
+	for (int x = 0, row = startRow; x < maxSteps && row < rowCount; x++)
 	{
-		for (int y = 0,col = startCol; y < maxSteps && col<colCount; y++)
+		for (int y = 0, col = startCol; y < maxSteps && col < colCount; y++)
 		{
 
 			const int topx = x * 10;
@@ -55,14 +58,14 @@ void HeroSVGRenderer::render(const Game& g) const
 			else
 			{
 				img = m->get(row, col) == Map::Wall ? g.getWallTexture()
-					: g.getFreeTexture();
+													: g.getFreeTexture();
 			}
 
 			if (img != "")
 			{
 				stream << "<image x=\"" << topy << "\" y=\"" << topx << "\" width=\"10\" height=\"10\" href=\""
-					<< img
-					<< "\" />\n";
+					   << img
+					   << "\" />\n";
 			}
 			else
 			{
